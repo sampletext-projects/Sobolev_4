e0aa069b48138e6cc02665286352461e5c8e733c
Bird Egop <sampletext32@bk.ru>
2021-03-14 00:13:11 +0300

    Add project

---
+++ Sobolev_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sobolev_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("z=");
+            double z = double.Parse(Console.ReadLine());
+
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+
+            double S = 0;
+            int key = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                S += (Math.Cos(Math.Pow(z, 2 * i)) + a) / Math.Sqrt(Math.Tan(Math.Pow(z, i)) + Math.Cos(Math.Pow(z, key)));
+                key--;
+            }
+            Console.WriteLine("Result=" + S);
+        }
+    }
+}
